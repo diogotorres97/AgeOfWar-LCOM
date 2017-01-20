bba8bff1335678b3099ca492623f24fc208ca255
diogotorres97 <diogo.rey@hotmail.com>
2017-01-20 20:15:06 +0000

    Delete compilation files

--- src/slot.d
+++
@@ -1,6 +0,0 @@
-
-slot.o:	bitmap.h
-slot.o:	const.h
-slot.o:	slot.c
-slot.o:	slot.h
-slot.o:	video_gr.h
--- src/terrain.d
+++
@@ -1,6 +0,0 @@
-
-terrain.o:	bitmap.h
-terrain.o:	const.h
-terrain.o:	terrain.c
-terrain.o:	terrain.h
-terrain.o:	video_gr.h
